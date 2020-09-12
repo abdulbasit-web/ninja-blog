2ed0c34bf5c9f042ebafe6a7c22363788124b778
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-12 06:13:00 +0300

    send db data to a view

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs')
 
 //connect to mongoDb
 const DB_URL =
-  'mongodb+srv://abdulbasit:42591800@cluster0.bghf6.mongodb.net/blog-db?retryWrites=true&w=majority'
+  'mongodb+srv://abdulbasit:42591800@cluster0.bghf6.mongodb.net/ninja-blog?retryWrites=true&w=majority'
 mongoose
   .connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(res => {
@@ -19,18 +19,18 @@ mongoose
   })
   .catch(err => console.log(err))
 
-//listen for requests
-app.listen(3000)
-
 //middleware and static files
 app.use(express.static('public'))
 // app.use(morgan('dev'))
 
+//listen for requests
+app.listen(3000)
+
 //mongoose and mongo sandbox route
 app.get('/add-blog', (req, resp) => {
   const blog = new Blog({
-    title: 'meren stack',
-    snippet: 'learn meren stack',
+    title: 'react',
+    snippet: 'learn react js',
     body:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt facere at adipisci quam molestiae laborum magnam voluptatibus laudantium iste fugiat dicta id, non inventore eligendi qui! Facilis amet deleniti inventore vel repudiandae nulla porro aliquam, eum sunt aperiam, sed corrupti.',
   })
@@ -40,14 +40,24 @@ app.get('/add-blog', (req, resp) => {
     .catch(err => console.log(err))
 })
 
-app.get('/', (req, resp) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ]
-  resp.render('index', {title: 'Home', blogs})
+app.get('/all-blog', (req, resp) => {
+  Blog.find()
+    .then(result => resp.send(result))
+    .catch(err => console.log(err))
 })
+
+app.get('/', (req, resp) => {
+  resp.redirect('/blogs')
+})
+
+//blog route
+app.get('/blogs', (req, resp) => {
+  Blog.find()
+    .sort({createdAt: -1})
+    .then(data => resp.render('index', {title: 'blogs', blogs: data}))
+    .catch(err => console.log(err))
+})
+
 app.get('/about', (req, resp) => resp.render('about', {title: 'About'}))
 //create
 app.get('/blogs/create', (req, resp) => resp.render('create', {title: ' Create new post'}))
