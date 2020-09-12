32c3ca8f07538ccdb291dad105d2cf290cae6215
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-12 05:24:55 +0300

    coonect to mongoDb && make model

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 //express app
 const app = express()
 
 //register view engine
 app.set('view engine', 'ejs')
+
+//connect to mongoDb
+const DB_URL =
+  'mongodb+srv://abdulbasit:42591800@cluster0.bghf6.mongodb.net/blog-db?retryWrites=true&w=majority'
+mongoose
+  .connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(res => {
+    console.log('connected')
+  })
+  .catch(err => console.log(err))
 
 //listen for requests
 app.listen(3000)
