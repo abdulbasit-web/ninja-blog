853c6699144556d018f21b4917576f1b7098a5c2
abdulbasit-web <abdullbasit99salah@gmail.com>
2020-09-12 08:51:42 +0300

    handle mongo route

--- server.js
+++ server.js
@@ -11,38 +11,29 @@ app.set('view engine', 'ejs')
 
 //connect to mongoDb
 const DB_URL =
-  'mongodb+srv://abdulbasit:42591800@cluster0.bghf6.mongodb.net/ninja-blog?retryWrites=true&w=majority'
-mongoose
-  .connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(res => {
-    console.log('connected')
-  })
-  .catch(err => console.log(err))
+  'mongodb+srv://abdulbasit:42591800@cluster0.bghf6.mongodb.net/ninja-blog?retryWrites=true&w=majority'mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+  console.log('connected')
+})
 
 //middleware and static files
 app.use(express.static('public'))
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 
 //listen for requests
 app.listen(3000)
 
-//mongoose and mongo sandbox route
+//mongoose and mongo sandbox routes
 app.get('/add-blog', (req, resp) => {
   const blog = new Blog({
-    title: 'react',
+    title: 'React js',
     snippet: 'learn react js',
     body:
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt facere at adipisci quam molestiae laborum magnam voluptatibus laudantium iste fugiat dicta id, non inventore eligendi qui! Facilis amet deleniti inventore vel repudiandae nulla porro aliquam, eum sunt aperiam, sed corrupti.',
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos quaerat voluptatem ipsum quas! A aliquam iusto hic? Et, quisquam maxime? Dolorem, explicabo?',
   })
+
   blog
     .save()
-    .then(result => resp.send(result))
-    .catch(err => console.log(err))
-})
-
-app.get('/all-blog', (req, resp) => {
-  Blog.find()
-    .then(result => resp.send(result))
+    .then(data => resp.send(data))
     .catch(err => console.log(err))
 })
 
@@ -52,10 +43,7 @@ app.get('/', (req, resp) => {
 
 //blog route
 app.get('/blogs', (req, resp) => {
-  Blog.find()
-    .sort({createdAt: -1})
-    .then(data => resp.render('index', {title: 'blogs', blogs: data}))
-    .catch(err => console.log(err))
+  Blog.find().then(data => resp.render('index', {title: 'blogs', blogs: data}))
 })
 
 app.get('/about', (req, resp) => resp.render('about', {title: 'About'}))
